75b883dd8734499e36796ed4dddc844734c24fb2
felipepeixoto33 <felipepeixoto33@hotmail.com>
2024-05-18 18:59:00 -0300

    Upload and download videos

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,4 +1,4 @@
-// src/api/upload.js
+// src/api/routes.js
 import express from 'express';
 import multer from 'multer';
 import fs from 'fs';
@@ -8,7 +8,7 @@ const router = express.Router();
 const upload = multer({ dest: 'uploads/' });
 
 // Endpoint to upload JSON files
-router.post('/upload', upload.single('file'), (req, res) => {
+router.post('/upload-json', upload.single('file'), (req, res) => {
     const tempPath = req.file.path;
     const targetPath = path.join(process.cwd(), 'src', 'public', 'emotion_data.json');
 
@@ -16,7 +16,7 @@ router.post('/upload', upload.single('file'), (req, res) => {
         fs.rename(tempPath, targetPath, err => {
             if (err) return res.sendStatus(500);
 
-            res.status(200).json({ message: 'File uploaded successfully', filename: req.file.originalname });
+            res.status(200).json({ message: 'JSON file uploaded successfully', filename: req.file.originalname });
         });
     } else {
         fs.unlink(tempPath, err => {
@@ -31,6 +31,36 @@ router.post('/upload', upload.single('file'), (req, res) => {
 router.get('/download-json', (req, res) => {
     const filePath = path.join(process.cwd(), 'src', 'public', 'emotion_data.json');
     res.download(filePath, 'emotion_data.json', (err) => {
+        if (err) {
+            res.status(500).send('Error downloading the file');
+        }
+    });
+});
+
+// Endpoint to upload video files
+router.post('/upload-video', upload.single('file'), (req, res) => {
+    const tempPath = req.file.path;
+    const targetPath = path.join(process.cwd(), 'src', 'public', 'uploaded_video.mp4');
+
+    if (req.file.mimetype.startsWith('video/')) {
+        fs.rename(tempPath, targetPath, err => {
+            if (err) return res.sendStatus(500);
+
+            res.status(200).json({ message: 'Video file uploaded successfully', filename: req.file.originalname });
+        });
+    } else {
+        fs.unlink(tempPath, err => {
+            if (err) return res.sendStatus(500);
+
+            res.status(400).json({ message: 'Only video files are allowed!' });
+        });
+    }
+});
+
+// Endpoint to download the video file
+router.get('/download-video', (req, res) => {
+    const filePath = path.join(process.cwd(), 'src', 'public', 'uploaded_video.mp4');
+    res.download(filePath, 'uploaded_video.mp4', (err) => {
         if (err) {
             res.status(500).send('Error downloading the file');
         }
--- src/server.js
+++ src/server.js
@@ -1,17 +1,18 @@
 // src/server.js
 import express from 'express';
-import apiRoutes from './api/routes.js';
 import path from 'path';
+import apiRoutes from './api/routes.js';
 
 const app = express();
 const port = 3000;
 
-app.use('/api', apiRoutes);
-app.use(express.static('src/public'));
-
+// Serve the index.html file at the root endpoint
 app.get('/', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'src', 'public', 'index.html'));
 });
+
+app.use('/api', apiRoutes);
+app.use(express.static('src/public'));
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
